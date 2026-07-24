c69085b571d88ed951a69872b0c862b229ac4ec4
EdwuardTSW <edwuard2213dk@gmail.com>
2026-07-24 10:48:27 -0600

    chore: inicializar backend con Spring Boot

---
+++ Ariscend_back/src/main/java/com/ariscend/backend/AriscendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ariscend.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriscendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriscendBackendApplication.class, args);
+	}
+
+}
